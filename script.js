d77de9ef54140561be84a9c1ece033a8e513aa6d
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-12-04 20:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+// script.js
+
+function delayMessage() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+async function updateText() {
+  const outputElement = document.getElementById('output');
+  
+  try {
+    const message = await delayMessage();
+    outputElement.textContent = message;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Call the function to update the text after the promise resolves
+updateText();
